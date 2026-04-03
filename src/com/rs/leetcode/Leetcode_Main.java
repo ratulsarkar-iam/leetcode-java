077dde3069681db892cd7fb9f676742c071fc13b
Ratul Sarkar <ratulsarkar@Ratuls-MacBook-Air.local>
2026-04-03 10:40:17 +0530

    Leetcode 74

--- src/com/rs/leetcode/Leetcode_Main.java
+++ src/com/rs/leetcode/Leetcode_Main.java
@@ -1,15 +1,19 @@
 package com.rs.leetcode;
 
 import com.rs.leetcode.easy.Leetcode_9;
+import com.rs.leetcode.med.Leetcode_151;
+import com.rs.leetcode.med.Leetcode_74;
 
 public class Leetcode_Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		Leetcode_204 leetcode_204= new Leetcode_204();
-		Leetcode_9 leetcode_9 = new Leetcode_9();
+		Leetcode_74 leet = new Leetcode_74 ();
+
 		System.out.println(Integer.MAX_VALUE);
-		System.out.println(leetcode_9.isPalindrome(0));
+		int[][] matrix={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+		System.out.println(leet.searchMatrix (matrix,34));
 
 	}
 
---
+++ src/com/rs/leetcode/med/Leetcode_74.java
@@ -0,0 +1,64 @@
+package com.rs.leetcode.med;
+
+/*
+* You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+*
+* Example 1:
+https://assets.leetcode.com/uploads/2020/10/05/mat.jpg
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+Example 2:
+
+https://assets.leetcode.com/uploads/2020/10/05/mat2.jpg
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+* */
+public class Leetcode_74 {
+    public boolean searchMatrix (int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int st = 0;
+        int end = m - 1;
+        while (st <= end) {
+            int midR = st + (end - st) / 2;
+//            System.out.println (matrix[midR][0] + " " + target + " " + matrix[midR][n - 1]);
+            if (matrix[midR][n - 1] >= target && matrix[midR][0] <= target) {
+                int stRs = 0;
+                int endRs = n - 1;
+                while (stRs <= endRs) {
+                    int midRs = stRs + (endRs - stRs) / 2;
+                    if (matrix[midR][midRs] == target) {
+                        return true;
+                    } else if (matrix[midR][midRs] < target) {
+                        stRs = midRs + 1;
+                    } else {
+                        endRs = midRs - 1;
+                    }
+                }
+                return false;
+            } else if (matrix[midR][n - 1] < target) {
+                st = midR + 1;
+            } else {
+                end = midR - 1;
+            }
+
+
+        }
+        return false;
+    }
+}
