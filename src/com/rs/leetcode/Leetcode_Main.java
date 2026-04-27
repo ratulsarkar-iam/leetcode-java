f1000261c5f57b04bebd13f8a1a839dfddd4fd6f
Ratul Sarkar <ratulsarkar@Ratuls-MacBook-Air.local>
2026-04-27 18:15:25 +0530

    updated

--- src/com/rs/leetcode/Leetcode_Main.java
+++ src/com/rs/leetcode/Leetcode_Main.java
@@ -1,12 +1,12 @@
 package com.rs.leetcode;
 
-import com.rs.leetcode.prc.L51_D1;
+import com.rs.leetcode.hard.Leetcode_51;
 
 public class Leetcode_Main {
 
     public static void main (String[] args) {
         // TODO Auto-generated method stub
-        L51_D1 leet = new L51_D1 ();
+        Leetcode_51 leet = new Leetcode_51 ();
         int[][] matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
         char[] s = new char[]{'h', 'e', 'l', 'l', 'o'};
         int[] nums = {1, 2, 3};
